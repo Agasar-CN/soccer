d952a8bd9eb8602e18529ddc12ff078b1259d12e
Agasar-CN <Agasar@qq.com>
2019-01-16 01:29:12 +0800

    soccer

---
+++ src/test/java/nchu/software/soccer/dao/UserDaoTest.java
@@ -0,0 +1,20 @@
+package nchu.software.soccer.dao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserDaoTest {
+
+    @Test
+    public void addUser() {
+    }
+
+    @Test
+    public void selectUser() {
+    }
+}
