004ba856dcfb9dec0da974374a76c3efc953ad63
Agasar-CN <Agasar@qq.com>
2019-01-16 19:59:00 +0800

    加入了日志配置，可以记录日志

--- src/main/java/nchu/software/soccer/controller/PrimaryTurnController.java
+++ src/main/java/nchu/software/soccer/controller/PrimaryTurnController.java
@@ -1,12 +1,20 @@
 package nchu.software.soccer.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class PrimaryTurnController {
+    //日志输出
+    private static final Logger logger=LoggerFactory.getLogger(PrimaryTurnController.class);
     @RequestMapping("/")
-    public String host(){
+    public String host(HttpServletRequest request) {
+        logger.info("用户访问:"+request.getSession().getCreationTime());
         return "index.html";
     }
 }
